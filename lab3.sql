723c9fb42712da8c9fdbc05dfbbfa1f73fc5535d
mollybauer1 <122919205+mollybauer1@users.noreply.github.com>
2023-09-13 18:01:50 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -21,3 +21,14 @@ select * from course
 --q.4
 insert into public.enroll_list(s_email, c_number) values ('s1@jmu.edu', 'c1'), ('s2@jmu.edu', 'c1'),
 ('s3@jmu.edu', 'c1'), ('s4@jmu.edu', 'c2'), ('s2@jmu.edu', 'c3'), ('s3@jmu.edu', 'c3')
+
+--q2.5
+-- insert profesor first because if you don't have p3, the course can not be inserted
+insert into public.professor(p_email,p_name,office)
+values ('p3@jmu.edu', 'p3', 'o3');
+insert into public.course(c_number, c_name, room, p_email) values ('c4', 'facebook', 'r1', 'p3@jmu.edu')
+
+-- q2.6
+UPDATE course
+set p_email = 'p3@jmu.edu' where p_email = 'p1@jmu.edu';
+delete from professor where p_name = 'p1'
