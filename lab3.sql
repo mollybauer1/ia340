4e2e3db025768014d42875305679b9a6b6be73b5
mollybauer1 <122919205+mollybauer1@users.noreply.github.com>
2023-09-13 17:14:48 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -6,3 +6,8 @@ values ('s1@jmu.edu', 's1', 'IA'),
   ('s4@jmu.edu', 's4', 'ISAT')
 
 select * from student 
+
+--q2.2
+insert into public.professor(p_email, p_name, office)
+values ('p1@jmu.edu', 'p1', 'o1'), ('p2@jmu.edu', 'p2', 'o2')
+select * from professor
